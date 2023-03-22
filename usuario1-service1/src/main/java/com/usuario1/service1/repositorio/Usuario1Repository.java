dbe7cf8c7428dafd9fdcc99618b4b5804f33a8f1
yuri sanchez <jhianpeeryurisr@gmail.com>
2023-03-22 07:40:41 -0500

    eureka

--- usuario1-service1/src/main/java/com/usuario1/service1/repositorio/Usuario1Repository.java
+++
@@ -1,9 +0,0 @@
-package com.usuario1.service1.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.usuario1.service1.entidades.Usuario01;
-
-public interface Usuario1Repository extends JpaRepository<Usuario01, Integer>{
-
-}
