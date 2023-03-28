5ea04c70a7718a7bc61520a3b2a8beb26db6e36d
yuri sanchez <jhianpeeryurisr@gmail.com>
2023-03-28 18:02:11 -0500

    zipkin

--- usuario-service/src/main/java/com/usuario/service/controlador/UsuarioController.java
+++ usuario-service/src/main/java/com/usuario/service/controlador/UsuarioController.java
@@ -100,30 +100,36 @@ public class UsuarioController {
 	private ResponseEntity<List<Carro>> fallBackGetCarros(@PathVariable("usuarioId") int id,
 			RuntimeException excepcion) {
 		return new ResponseEntity("El usuario : " + id + "tiene los carros en el taller", HttpStatus.OK);
+		
 
 	}
 
 	private ResponseEntity<Carro> fallBackSaveCarros(@PathVariable("usuarioId") int id, @RequestBody Carro carro,
 			RuntimeException excepcion) {
 		return new ResponseEntity("El usuario : " + id + "No tiene dinero para los carros", HttpStatus.OK);
-
+   
+   
 	}
 
 	private ResponseEntity<List<Moto>> fallBackGetMotos(@PathVariable("usuarioId") int id, RuntimeException excepcion) {
 		return new ResponseEntity("El usuario : " + id + "tiene las motos en el taller", HttpStatus.OK);
 
+	
 	}
+	
 
 	private ResponseEntity<Moto> fallBackSaveMoto(@PathVariable("usuarioId") int id, @RequestBody Moto moto,
 			RuntimeException excepcion) {
 		return new ResponseEntity("El usuario : " + id + "No tiene dinero para las motos", HttpStatus.OK);
-
+    
+   
 	}
 
 	private ResponseEntity<Map<String, Object>> fallBackGetTodos(@PathVariable("usuarioId") int id,
 			RuntimeException excepcion) {
 		return new ResponseEntity("El usuario : " + id + "tiene los vehiculos en el taller", HttpStatus.OK);
-
+	
+	
 	}
 	*/
 	private ResponseEntity<List<Carro>> fallBackGetCarros(@PathVariable("usuarioId") int id,
